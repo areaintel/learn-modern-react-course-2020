76899b4ed6387d5d8d23405bf9191d8a46e94ca2
areaintel <hbastow@gmail.com>
2020-04-14 15:19:01 -0700

    completion of video 3

---
+++ weather/src/components/WeatherCard/component.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+import Location from './Location';
+
+const WeatherCard = (props) => {
+
+    const red = 100;
+    const Card = styled.div`
+        margin: 0 auto;
+        background: linear-gradient(to bottom,rgba(${red},200,200),lightblue);
+        width: 200px;
+        height: 240px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+        border-radius: 15px;
+    `
+
+    return ( 
+        <Card>
+            <Location/>
+            <img className="icon" src="./img/Mostly Cloudy-2x.png" alt="Weather Icon"/>
+            <h1 className="temp">20 C</h1>
+            <h3 className="condition">Clouds</h3>
+        </Card>
+     );
+}
+ 
+export default WeatherCard;
