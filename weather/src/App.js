84ef0072c5aba08895ca094e0e5e5fb0eb56ac54
areaintel <hbastow@gmail.com>
2020-04-15 12:21:03 -0700

    start of consuming of weather API, completion of video 7

--- weather/src/App.js
+++ weather/src/App.js
@@ -4,6 +4,19 @@ import WeatherCard from "./components/WeatherCard/component";
 import "./App.css";
 
 function App() {
+  const data = async () => {
+    const apiRes = await fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=London,gb&units=metric&APPID=9f918212005cbe0e90937fd03352fb4d"
+    );
+    const resJSON = await apiRes.json();
+    return resJSON;
+  };
+  //console.log(data());
+  data().then((res) => {
+    console.log("the feels like is" + res.main.feels_like);
+    console.log("the temp is" + res.main.temp);
+  });
+
   return (
     <div className="App">
       <WeatherCard temp={-5} condition="Clear" city="Sydney" country="AU" />
